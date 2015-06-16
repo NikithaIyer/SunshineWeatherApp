c10e7db7027ea4a1761ffde59234f517d6b29ea0
Nikitha Iyer <nikita9.iyer@gmail.com>
2015-06-17 00:30:56 +0530

    Implementing ListView with ArrayAdapter

--- app/src/main/java/nikithaiyer/com/sunshineweatherapp/MainActivity.java
+++ app/src/main/java/nikithaiyer/com/sunshineweatherapp/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -47,12 +53,26 @@ public class MainActivity extends ActionBarActivity {
   }
 
   public static class PlaceholderFragment extends Fragment {
+
+    private ArrayAdapter<String> adapter;
+
     public PlaceholderFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       View rootView = inflater.inflate(R.layout.fragment_main, container,false);
+      String[] forecastArray = { "Today Sunny",
+                                "Tomorrow Foggy",
+                                "Wednesday Rain",
+                                "Thursday Mist",
+                                "Friday Snowfall",
+                                "Saturday Cloudy",
+                                "Sunday Rain" };
+      ArrayList<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+      ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
+      adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+      listView.setAdapter(adapter);
       return rootView;
     }
   }
