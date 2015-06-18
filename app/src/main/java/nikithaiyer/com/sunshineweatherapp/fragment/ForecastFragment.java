8d59b48a3d94d0af44203e1689d7c570723c2954
Nikitha Iyer <nikita9.iyer@gmail.com>
2015-06-18 23:48:04 +0530

    Fetching data from server during startup as well. Note: Have to create new Async task for every request i.e onCreate one should be different than the refresh one

--- app/src/main/java/nikithaiyer/com/sunshineweatherapp/fragment/ForecastFragment.java
+++ app/src/main/java/nikithaiyer/com/sunshineweatherapp/fragment/ForecastFragment.java
@@ -12,7 +12,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import nikithaiyer.com.sunshineweatherapp.R;
@@ -22,15 +21,7 @@ import nikithaiyer.com.sunshineweatherapp.network.FetchWeatherTask;
 public class ForecastFragment extends Fragment implements WeatherAsyncResponse {
 
   private ArrayAdapter<String> adapter;
-  private final String[] forecastArray = new String[]{"Today Sunny",
-      "Tomorrow Foggy",
-      "Wednesday Rain",
-      "Thursday Mist",
-      "Friday Snowfall",
-      "Saturday Cloudy",
-      "Sunday Rain"};
   private ListView listView;
-  //  private String[] forecastArray = new String[]{};
 
   public ForecastFragment() {
   }
@@ -44,10 +35,16 @@ public class ForecastFragment extends Fragment implements WeatherAsyncResponse {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.forecast_fragment, container, false);
-    List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
-    listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
+
+    FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+    fetchWeatherTask.weatherAsyncResponse = this;
+
+    List<String> weekForecast = new ArrayList<>();
     adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
-    listView.setAdapter(adapter);
+    listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
+    fetchWeatherTask.execute("411011");
+
+//    listView.setAdapter(adapter);
     return rootView;
   }
 
@@ -62,8 +59,8 @@ public class ForecastFragment extends Fragment implements WeatherAsyncResponse {
     int id = item.getItemId();
     if (id == R.id.action_refresh) {
       FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-      fetchWeatherTask.execute("411011");
       fetchWeatherTask.weatherAsyncResponse = this;
+      fetchWeatherTask.execute("411011");
       return true;
     }
     return super.onOptionsItemSelected(item);
